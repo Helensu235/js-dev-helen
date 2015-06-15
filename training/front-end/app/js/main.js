4ad4b97f6a44e3c6012b402f26b45c4484f0834a
unknown <Helen Tamayo@HTAMAYOA-DH01.jalasoft.local>
2015-06-15 16:05:37 -0400

    This is the commit for the first task. This is the commit for the first task. This is the commit for the first task.

--- training/front-end/app/js/main.js
+++ training/front-end/app/js/main.js
@@ -1,1 +1,54 @@
-helensu
+
+var sum=function(n,pos){
+    if(pos==undefined)
+        pos=n.length-1;
+    if(pos==0)
+        return n[0];
+    return n[pos]+sum(n,pos-1);
+
+};
+
+var avg=function(n,pos){
+    if(pos==undefined)
+        pos=n.length-1;
+    if(pos==0)
+        return n[0];
+    return(n[pos]+sum(n,pos-1))/(n.length);
+
+};
+
+var maximum = function(lista,pos,n){
+    if(pos==undefined)
+        pos=lista.length-1;
+    if(pos==0 && n==undefined)
+        return lista[0];
+    if(n==undefined)
+        n = lista[pos];
+    if(pos<0)
+        return n;
+    if(lista [pos]>n)
+        n=lista [pos];
+    return maximum(lista,pos-1,n);
+};
+
+var minimum = function(lista,pos,n){
+    if(pos==undefined)
+        pos=lista.length-1;
+    if(pos==0 && n==undefined)
+        return lista[0];
+    if(n==undefined)
+        n = lista[pos];
+    if(pos<0)
+        return n;
+    if(lista [pos]<n)
+        n=lista [pos];
+    return minimum(lista,pos-1,n);
+};
+
+
+var calculateValues=function(){
+    console.log('SUM',sum(arguments));
+    console.log('AVG',avg(arguments));
+    console.log('MAX',maximum(arguments));
+    console.log('MIN',minimum(arguments));
+};
